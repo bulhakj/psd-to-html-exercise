c9c0fe4c83d688008e02d798cbae282f0ad037b9
bulhakj <jakub.bulhak@gmail.com>
2019-02-14 16:17:41 +0100

    Added smooth scroll to menu items. Added scroll to top button. Fixed problem with extending home section while opening hamburger menu on mobile devices.

--- scripts.js
+++ scripts.js
@@ -1,11 +1,48 @@
+$(document).ready(function scrolling() {
+
+    $('a[href^="#"]').on('click', function(event) {
+
+        var target = $($(this).attr('href'));
+
+        if (target.length) {
+            event.preventDefault();
+            $('html, body').animate({
+                scrollTop: target.offset().top
+            }, 1000);
+        }
+    });
+  });
+
+$('a.back-to-top').click(function() {
+  $('html, body').animate({
+      scrollTop: 0
+  }, 1000);
+  return false;
+});
+
+let topBtn = document.getElementById("scroll-to-top");
+
+window.addEventListener('scroll', scrollFunction);
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    topBtn.style.display = "block";
+  } else {
+    topBtn.style.display = "none";
+  }
+}
+
 function menu() {
-    var items = document.querySelector("#menu-list");
+    const items = document.querySelector("#menu-list");
     if(items.className === "menu-items-visible menu-items-ham") {
         items.className = "menu-items-ham";
     } else {
         items.className = "menu-items-visible menu-items-ham";
-    }  
+    }
 }
 
 const hamburger = document.querySelector(".hamburger-menu");
 hamburger.addEventListener("click", menu);
+
+
+
+
